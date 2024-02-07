347055b6d462f827f4619ec6bd6547dc5e26cc9e
BryceFuerty <95210822+BryceFuerty@users.noreply.github.com>
2024-02-07 08:41:20 +0100

    Update Img

--- src/components/banner/RightBanner.js
+++ src/components/banner/RightBanner.js
@@ -5,7 +5,7 @@ const RightBanner = () => {
   return (
     <div className="w-full lgl:w-1/2 flex justify-center items-center relative" style={{paddingBottom:"100px"}}>
       <img
-        className="w-[30%] h-[40%] lgl:w-[500px] lgl:h-[680px] z-10"
+        className="w-[30%] h-[40%]  z-10"
         src={bannerImg}
         alt="bannerImg"
         style={{borderRadius:"10px"}}
